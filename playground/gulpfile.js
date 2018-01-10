1d62d90aef9d029e1ff503437151a6d27a3f9edb
Amber Brookhuis <amberbrookhuis@gmail.com>
2018-01-10 10:14:46 +0100

    And now the right gulpfile.js

--- playground/gulpfile.js
+++ playground/gulpfile.js
@@ -8,27 +8,20 @@ var sass         = require('gulp-sass'),
     rename       = require('gulp-rename'),
     mmq          = require('gulp-merge-media-queries');
 
-gulp.task("default", ['stylesheets', 'fonts']);
+gulp.task("default", ['stylesheets']);
 
-// Compile Stylesheets
 gulp.task('stylesheets', function() {
-    return gulp.src('./Client/stylesheets/**/*.scss')
+    return gulp.src('./Client/stylesheets/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(mmq({log: false}))
         .pipe(autoprefixer({browsers: ['> 2%', 'last 2 versions'], cascade: false}))
         .pipe(gulp.dest("./wwwroot/css"))
-        .pipe(rename({ suffix: '.min' }))
+      .pipe(rename({ suffix: '.min' }))
         .pipe(cleanCSS())
         .pipe(gulp.dest("./wwwroot/css"))
 });
 
-// Compile fonts
-gulp.task('fonts', function() {
-    return gulp.src('./Client/fonts/**/*')
-        .pipe(gulp.dest("./wwwroot/fonts"))
-});
-
 // Watch Stylesheets
 gulp.task('watch', function(callback) {
-    gulp.watch('./Client/stylesheets/**/*.scss', ['stylesheets', 'fonts']);
+    gulp.watch(['./Client/stylesheets/**/*.scss'], ['stylesheets']);
 });
