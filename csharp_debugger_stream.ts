5e397c9a72031d49506802b1376e011e9eb87d1c
mabbadi <mabbadi@outlook.com>
2018-01-23 10:46:37 +0100

    added AST to DebuggerStream

--- csharp_debugger_stream.ts
+++ csharp_debugger_stream.ts
@@ -11,8 +11,15 @@ import * as Py from "./Python/python"
 import * as CSharp from "./CSharpTypeChecker/csharp"
 import { co_run_to_end } from "./ccc_aux";
 import { ast_to_type_checker } from "./CSharpTypeChecker/csharp";
+import { Stmt } from "./main";
 
-export type DebuggerStream = ({ kind:"error"|"done" } | { kind:"step", next:() => DebuggerStream }) & { show:() => { kind:"memory", memory:Py.MemRt } | { kind:"bindings", state:CSharp.State } | { kind:"message", message:string } }
+export type DebuggerStream = 
+  ({ kind:"error"|"done" } | 
+  { kind:"step", next:() => DebuggerStream }) & 
+  { show:() => { kind:"memory", memory:Py.MemRt, ast:Stmt } | 
+               { kind:"bindings", state:CSharp.State, ast:Stmt } | 
+               { kind:"message", message:string } }
+
 export let get_stream = (source:string) : DebuggerStream => {
   let parse_result = CSharp.GrammarBasics.tokenize(source)
   if (parse_result.kind == "left") {
@@ -29,7 +36,7 @@ export let get_stream = (source:string) : DebuggerStream => {
 
   let p = ast_to_type_checker(res.value.fst)
 
-  let runtime_stream = (state:Prod<Py.StmtRt,Py.MemRt>) : DebuggerStream => ({
+  let runtime_stream = (state:Prod<Py.StmtRt,Py.MemRt>, ast:Stmt) : DebuggerStream => ({
     kind:"step",
     next:() => {
       let p = state.fst
@@ -40,15 +47,15 @@ export let get_stream = (source:string) : DebuggerStream => {
         return { kind:"error", show:() => ({ kind:"message", message:error }) }
       }
       if (k.value.kind == "left") {
-        return runtime_stream(k.value.value)
+        return runtime_stream(k.value.value, ast)
       }
       s = k.value.value.snd
-      return { kind:"done", show:() => ({ kind:"memory", memory:s}) }
+      return { kind:"done", show:() => ({ kind:"memory", memory:s, ast:ast}) }
     },
-    show:() => ({ kind:"memory", memory:state.snd })
+    show:() => ({ kind:"memory", memory:state.snd, ast:ast })
   })
 
-  let typechecker_stream = (state:Prod<CSharp.Stmt,CSharp.State>) : DebuggerStream => ({
+  let typechecker_stream = (state:Prod<CSharp.Stmt,CSharp.State>, ast:Stmt) : DebuggerStream => ({
     kind:"step",
     next:() => {
       let p = state.fst
@@ -59,15 +66,15 @@ export let get_stream = (source:string) : DebuggerStream => {
         return { kind:"error", show:() => ({ kind:"message", message:error }) }
       }
       if (k.value.kind == "left") {
-        return typechecker_stream(k.value.value)
+        return typechecker_stream(k.value.value, ast)
       }
       let initial_runtime_state = apply(constant<Unit,Py.StmtRt>(k.value.value.fst.sem).times(constant<Unit,Py.MemRt>(Py.empty_memory_rt)), {})
-      return runtime_stream(initial_runtime_state)
+      return runtime_stream(initial_runtime_state, ast)
     },
-    show:() => ({ kind:"bindings", state:state.snd })
+    show:() => ({ kind:"bindings", state:state.snd, ast:ast })
   })
 
   let initial_compiler_state = apply(constant<Unit,CSharp.Stmt>(p).times(constant<Unit,CSharp.State>(CSharp.empty_state)), {})
-  return typechecker_stream(initial_compiler_state)
+  return typechecker_stream(initial_compiler_state, p)
 }
 
